e1137663991a42b04441f2970758df569123af1c
Amed Reyes <reyesrodriguezjaime@gmail.com>
2025-08-15 15:17:33 -0600

    logic complete

--- dfa1.py
+++ dfa1.py
@@ -2,11 +2,14 @@ def printInfo():
 	print("DFA Sample 1 - by x86coder")
 	print(" > Input your string to be recognized:")
 	
-Q = [0, 1, 2]		# Finite set of states
-S = ['a', 'b']		# Input alphabet
-s = 0			# Initial state
-d = {'a' : 3, 'b' : 2}	# Delta function f(x)
-F = [1, 2]		# Set of acception states (python list object)
+Q = [0, 1, 2]		# Finite set of states (Q)
+S = ['a', 'b']		# Input alphabet (sigma)
+s = 0				# Initial state
+d = [[1,2], [1,2], [1,2]]	# Delta function f(x).
+					# 	values = [[state0], [state1], [state2], [[index for 'a', index for 'b']] ... [state N]]
+F = [2]				# Set of acception states (python list object)
+
+character_map = {'a' : 0, 'b' : 1}	# Python dictionary mapping to second-level array indexes
 
 printInfo()
 mystring = input()
@@ -16,9 +19,11 @@ word = list(mystring)	# Input string to the DFA (with each character separated)
 i = 0		# Loop control variable
 q = s		# DFA current state
 while i < len(mystring):
-	#print(d[word[i]])
-	q = d[word[i]]
-	i = i +1
+	secondary_index = character_map[word[i]]
+	#print("\t* secondary_index: " + str(secondary_index))
+	new_state = d[q][secondary_index] # Access transition pair in set Q x S. Old/current state is in q.
+	q = new_state
+	i = i+1
 	
 print("> Ending state is: q" + str(q))
 
